9f39612ee33c02d00c5fd63552289a8eca195eb2
Enofeverything <jamal0abc@gmail.com>
2020-06-02 07:54:24 +0000

    i just update it

--- config.js
+++ config.js
@@ -1,19 +1,15 @@
-exports.TOKEN = '';
+exports.TOKEN = 'NzE1MzQwMzY4MjI4NjQ2OTYy.XtX51w.lN8kqOK_ZyS_NVJhBS9ySHeWM18';
 
 exports.DBL_API_KEY = '';
 
-exports.PREFIX = '';
+exports.PREFIX = '-';
 
-exports.GOOGLE_API_KEY = '';
+exports.GOOGLE_API_KEY = 'AIzaSyAllsDH_DIQ-TP1PZt2ET3DPuOcq9HdiSc';
 
-exports.GENIUS_API_KEY = '';
+exports.GENIUS_API_KEY = '0oXy3R_J2PlN3T5Y5fJXk7Tcop6QslL4fkFjPO0ccZp9E2CvKbhxl7_7PjdhvsGZ';
 
-exports.yandex_API = '';
+exports.giphy_API = 'ZHGcrdW2kQs5Fb5haRIK4ylMFtZKXFi0';
 
-exports.news_API = '';
-
-exports.giphy_API = '';
-
-exports.AME_API = '';
+exports.AME_API = '871d38c2aef35f76a92542e9e78bbd57093df649db20c1d32a2402edfee1aa284e7dfb8fdce0ae532a912543a3d50188c62a80f1471d873ac76c8cc065346e16';
 
 exports.blague_API = '';
